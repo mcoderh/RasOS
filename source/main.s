012436d7f13eb01965c90eaaa50b2fb8574006a3
silencerh <renhao.zju@gmail.com>
2016-11-24 20:46:32 +0100

    BakingPi Lesson 5 done

--- source/main.s
+++ source/main.s
@@ -16,32 +16,37 @@ bl SetGpioFunction
 .unreq pinNum
 .unreq pinFunc
 
+ptrn .req r4
+ldr ptrn, =pattern
+ldr ptrn, [ptrn]
+seq .req r5
+mov seq,#0
+
+
 
 loop$:
+	mov r1, #1
+	lsl r1, seq
+	and r1, ptrn
+	teq seq, #31
+	moveq seq, #0
+	addne seq, #1
+
 	pinNum .req r0
 	pinVal .req r1
 	mov pinNum, #47
-	mov pinVal, #0
 	bl SetGpio
 	.unreq pinNum
 	.unreq pinVal
 
 	delay .req r0
-	ldr delay, =100000
+	ldr delay, =250000
 	bl Wait
-	.unreq delay
-
-	pinNum .req r0
-	pinVal .req r1
-	mov pinNum, #47
-	mov pinVal, #1
-	bl SetGpio
-	.unreq pinNum
-	.unreq pinVal
-
-	delay .req r0
-	ldr delay, =100000
-	bl Wait
-	.unreq delay
+	.unreq delay	
 
 b loop$
+
+.section .data
+.align 2
+pattern:
+.int 0b11111111101010100010001000101010
