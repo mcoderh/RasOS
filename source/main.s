d0f627eefffca48127f24b3857a8e27c7d893d82
silencerh <renhao.zju@gmail.com>
2016-11-24 19:27:16 +0100

    BakingPi lesson 3 done

--- source/main.s
+++ source/main.s
@@ -1,16 +1,30 @@
 .section .init
-.global _start
+.globl _start
 _start:
 
-ldr r0, =0x3f200000
-mov r1, #1
-lsl r1, #21
-str r1, [r0, #16]
+b main
+
+.section .text
+main:
+mov sp,#0x8000
+
+pinNum .req r0
+pinFunc .req r1
+mov pinNum,#47
+mov pinFunc, #1
+bl SetGpioFunction
+.unreq pinNum
+.unreq pinFunc
+
 
 loop$:
-	mov r1, #1
-	lsl r1, #15
-	str r1, [r0, #32]
+	pinNum .req r0
+	pinVal .req r1
+	mov pinNum, #47
+	mov pinVal, #0
+	bl SetGpio
+	.unreq pinNum
+	.unreq pinVal
 
 	mov r2, #0x3F0000
 	wait1$:
@@ -18,7 +32,13 @@ loop$:
 		cmp r2, #0
 	bne wait1$
 
-	str r1, [r0, #44]
+	pinNum .req r0
+	pinVal .req r1
+	mov pinNum, #47
+	mov pinVal, #1
+	bl SetGpio
+	.unreq pinNum
+	.unreq pinVal
 
 	mov r2, #0x3F0000
 	wait2$:
